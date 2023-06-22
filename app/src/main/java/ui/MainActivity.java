66c8da36ddd8f1401701ee2f809dbdfb75510fcd
cty <chen0914@algonquinlive.com>
2023-06-22 18:54:57 -0400

    Week4

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,81 +1,101 @@
 package ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+import static android.content.SharedPreferences.*;
 
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 import algonquin.cst2335.chen0914.R;
-import algonquin.cst2335.chen0914.databinding.ActivityMainBinding;
-import data.MainViewModel;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //private ActivityMainBinding variableBinding;
-    //private MainViewModel model;
+    //    protected ActivityMainBinding binding;
+    private String TAG = "MainActivity";
 
-    ImageView imageView;
-    Switch sw;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.flagview);
-        sw = findViewById(R.id.switch1);
-
-        sw.setOnCheckedChangeListener( (btn,isChecked)-> {
-            if (isChecked){
-                RotateAnimation rotate = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imageView.startAnimation(rotate);
-            }
-                else {
-                    imageView.clearAnimation();
-            }
-        });
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        prefs.getString("LoginName", "");
 
 
 
 
 
 
-        //model = new ViewModelProvider(this).get(MainViewModel.class);
+        Button button = findViewById(R.id.loginButton);
+        button.setOnClickListener( (v) -> {
+            Log.e("MainActivity", "You clicked the button");
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            EditText emailEditText = findViewById(R.id.emailText12);
+            String emailAddress = emailEditText.getText().toString();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName",emailAddress);
+            editor.apply();
+            nextPage.putExtra("emailAddress",emailAddress);
+            startActivity(nextPage);
 
-        //variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(variableBinding.getRoot());
-
-
-
-
-
-
-
-
-
+        } );
 
 
 
 
     }
 
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        setContentView(R.layout.activity_main);
+        Log.w("MainActivity", "In onStart() - The application is now visible on screen.");
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // setContentView(R.layout.activity_main);
+        Log.w("MainActivity", "In onPause() - The application no longer responds to user input.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //  setContentView(R.layout.activity_main);
+        Log.w("MainActivity", "In onResume() - The application is now responding to user input.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //  setContentView(R.layout.activity_main);
+        Log.w("MainActivity", "In onStop() - The application is no longer visible.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // setContentView(R.layout.activity_main);
+        Log.w("MainActivity", "In onDestroy() - Any memory used by the application is freed.");
+    }
+}
